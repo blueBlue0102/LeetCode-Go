e6c4cb7925506ffc53ea18170573fa86ad87dd26
Blue <blue.tzuhua@gmail.com>
2024-04-09 17:52:46 +0800

    206. Reverse Linked List - add recursion method

--- leetcode/0206.Reverse-Linked-List/206. Reverse Linked List.go
+++ leetcode/0206.Reverse-Linked-List/206. Reverse Linked List.go
@@ -5,6 +5,14 @@ import (
 )
 
 func ReverseLinkedList(head *structures.ListNode) *structures.ListNode {
+	if false {
+		return reverseLinkedList_twoPointer(head)
+	} else {
+		return reverseLinkedList_recursion(head)
+	}
+}
+
+func reverseLinkedList_twoPointer(head *structures.ListNode) *structures.ListNode {
 	var prev *structures.ListNode = nil
 	for head != nil {
 		next := head.Next
@@ -13,4 +21,15 @@ func ReverseLinkedList(head *structures.ListNode) *structures.ListNode {
 		head = next
 	}
 	return prev
+}
+
+func reverseLinkedList_recursion(head *structures.ListNode) *structures.ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	} else {
+		node := reverseLinkedList_recursion(head.Next)
+		head.Next.Next = head
+		head.Next = nil
+		return node
+	}
 }
--- leetcode/0206.Reverse-Linked-List/README.md
+++ leetcode/0206.Reverse-Linked-List/README.md
@@ -2,6 +2,12 @@
 
 <https://leetcode.com/problems/reverse-linked-list/>
 
+這題是 Linked List 的經典題  
+有兩種做法，分別是雙指標或是遞迴
+
+雙指標的時間複雜度是 $O(n)$；空間複雜度是 $O(1)$  
+遞迴的時間複雜度是 $O(n)$；空間複雜度是 $O(n)$
+
 ## Takeaway
 
 無
