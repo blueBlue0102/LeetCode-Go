2dfcec42ca0fec20715999b23cf8d643c7a15501
Blue <blue.tzuhua@gmail.com>
2024-04-18 15:18:13 +0800

    update: 75. Sort Colors

--- leetcode/0075.Sort-Colors/75. Sort Colors.go
+++ leetcode/0075.Sort-Colors/75. Sort Colors.go
@@ -1,28 +1,18 @@
 package leetcode
 
 func SortColors(nums []int) {
-	const MIN = 0
-	const MAX = 2
-
-	left := -1
-	mid := 0
-	right := len(nums)
+	const MIN, MAX = 0, 2
+	left, mid, right := -1, 0, len(nums)
 	for mid < right {
 		if nums[mid] == MIN {
 			left++
-			n := nums[mid]
-			nums[mid] = nums[left]
-			nums[left] = n
-
+			nums[mid], nums[left] = nums[left], nums[mid]
 			mid++
 		} else if nums[mid] == MAX {
 			right--
-			n := nums[mid]
-			nums[mid] = nums[right]
-			nums[right] = n
+			nums[mid], nums[right] = nums[right], nums[mid]
 		} else {
 			mid++
 		}
-
 	}
 }
