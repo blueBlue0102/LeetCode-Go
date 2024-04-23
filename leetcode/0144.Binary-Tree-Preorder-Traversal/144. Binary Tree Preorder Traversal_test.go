1543c732e0f9db56f531cbdc3a92345e04645214
Blue <blue.tzuhua@gmail.com>
2024-04-23 11:38:29 +0800

    144. Binary Tree Preorder Traversal

---
+++ leetcode/0144.Binary-Tree-Preorder-Traversal/144. Binary Tree Preorder Traversal_test.go
@@ -0,0 +1,46 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/blueBlue0102/LeetCode-Go/structures"
+)
+
+// 填入 function input type
+type parameters0144 struct {
+	root *structures.TreeNode
+}
+
+func TestBinaryTreePreorderTraversal(t *testing.T) {
+	tests := []struct {
+		parameters0144
+		// 填入 function output type
+		ans []int
+	}{
+		// 填入 test case
+		{
+			parameters0144{structures.Ints2TreeNode([]int{1, structures.NULL, 2, 3})},
+			[]int{1, 2, 3},
+		},
+		{
+			parameters0144{structures.Ints2TreeNode([]int{})},
+			[]int{},
+		},
+		{
+			parameters0144{structures.Ints2TreeNode([]int{1})},
+			[]int{1},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("Test BinaryTreePreorderTraversal", func(t *testing.T) {
+			// 完整輸入參數
+			result := BinaryTreePreorderTraversal(test.parameters0144.root)
+			// compare 的方式需視情況調整
+			if !reflect.DeepEqual(result, test.ans) {
+				t.Errorf("BinaryTreePreorderTraversal(%+v) got %+v, want %+v", test.parameters0144, result, test.ans)
+			}
+		})
+	}
+}
---
+++ leetcode/0144.Binary-Tree-Preorder-Traversal/README.md
@@ -0,0 +1,14 @@
+# 144. Binary Tree Preorder Traversal
+
+<https://leetcode.com/problems/binary-tree-preorder-traversal/>
+
+可以使用遞迴或 stack 的方式進行 traversal  
+時間複雜度和空間複雜度都是 $O(n)$
+
+或是可以使用 Morris Traversal 來達成時間複雜度 $O(n)$ 和空間複雜度 $O(1)$
+
+目前僅使用遞迴
+
+## Takeaway
+
+- Tree Preorder Traversal
