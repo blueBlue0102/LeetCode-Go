8afa2f9c1aadde84f2dfb905a77a67922c811268
Blue <blue.tzuhua@gmail.com>
2024-05-23 13:53:26 +0800

    242. Valid Anagram

--- leetcode/0242.Valid-Anagram/242. Valid Anagram.go
+++ leetcode/0242.Valid-Anagram/242. Valid Anagram.go
@@ -5,18 +5,17 @@ func ValidAnagram(s string, t string) bool {
 		return false
 	}
 
-	dictMap := make(map[rune]int)
-	for _, runeValue := range s {
-		dictMap[runeValue]++
-	}
-	for _, runeValue := range t {
-		dictMap[runeValue]--
-	}
+	dict := map[rune]int{}
 
-	for _, value := range dictMap {
-		if value != 0 {
-			return false
+	for _, r := range s {
+		dict[r]++
+	}
+	for _, r := range t {
+		dict[r]--
+		if dict[r] == 0 {
+			delete(dict, r)
 		}
 	}
-	return true
+
+	return len(dict) == 0
 }
