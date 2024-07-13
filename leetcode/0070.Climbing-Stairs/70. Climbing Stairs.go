db98e57103c37505aae1c7c34da4ed059922d6d9
Tzu-Hua Lan <blue.tzuhua@gmail.com>
2024-07-13 10:38:50 +0800

    70. Climbing Stairs: add iterate method

--- leetcode/0070.Climbing-Stairs/70. Climbing Stairs.go
+++ leetcode/0070.Climbing-Stairs/70. Climbing Stairs.go
@@ -1,29 +1,54 @@
 package leetcode
 
-var data = make(map[int]int)
-
 func ClimbingStairs(n int) int {
+	// return recur(n)
+	return iterate(n)
+}
+
+func recur(n int) int {
+	var data = map[int]int{}
+	var recurInterval func(n int) int
+	recurInterval = func(n int) int {
+		if n == 1 {
+			return 1
+		} else if n == 2 {
+			return 2
+		}
+
+		var nDown1 int
+		var nDown2 int
+
+		if val, existed := data[n-1]; existed {
+			nDown1 = val
+		} else {
+			data[n-1] = ClimbingStairs(n - 1)
+			nDown1 = data[n-1]
+		}
+		if val, existed := data[n-2]; existed {
+			nDown2 = val
+		} else {
+			data[n-2] = ClimbingStairs(n - 2)
+			nDown2 = data[n-2]
+		}
+
+		return nDown1 + nDown2
+	}
+	return recurInterval(n)
+}
+
+func iterate(n int) int {
 	if n == 1 {
 		return 1
 	} else if n == 2 {
 		return 2
 	}
 
-	var nDown1 int
-	var nDown2 int
+	v1, v2, v3 := 1, 2, 0
 
-	if val, existed := data[n-1]; existed {
-		nDown1 = val
-	} else {
-		data[n-1] = ClimbingStairs(n - 1)
-		nDown1 = data[n-1]
-	}
-	if val, existed := data[n-2]; existed {
-		nDown2 = val
-	} else {
-		data[n-2] = ClimbingStairs(n - 2)
-		nDown2 = data[n-2]
+	for i := 2; i < n; i++ {
+		v3 = v1 + v2
+		v1, v2 = v2, v3
 	}
 
-	return nDown1 + nDown2
+	return v3
 }
